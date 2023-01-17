d0e26cd5cc7f99f5f4a99e5eb1308f1cb44a3992
MinoHaluska <mh888mh@gmail.com>
2023-01-17 14:32:27 +0100

    main commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,95 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '01-ToDoList';
+
+  toDoForm = new FormGroup({
+    task: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(25)
+    ])
+  })
+  
+  todos: Task[] = []
+  taskDone: number = 0
+  color: string = 'warn'
+  progress: number = 0
+
+  constructor(){
+  }
+
+  resetForm(){
+    this.toDoForm.reset()
+  }
+  
+  ngOnInit(): void {
+    this.todos = [
+      { content: 'First task', completed: false },
+      { content: 'Second task', completed: false }
+    ]
+  }
+  
+  // Odoslanie Formulara
+  onSubmit(){
+    this.todos.push({
+      content: this.toDoForm.value.task,
+      completed: false
+    })
+
+    this.toDoForm.reset()
+  }
+
+  // Odstranenie ulohy
+  deleteTask(index: number){
+    this.todos = this.todos.filter((v, i) => i !== index)
+
+    // Prebehne pole a spocita kolko je hotovych uloch
+    let counterDone = 0
+    this.todos.forEach((todo) => {
+      if(todo.completed){
+        counterDone++
+      }
+    })
+
+    this.taskDone = counterDone
+    this.progress = ((this.taskDone / this.todos.length) * 100)
+  }
+
+  // Hotovo
+  doneTask(index: number){
+    
+    this.todos.map((v, i) => {
+      if(i === index){
+        v.completed = !v.completed
+        return v
+      }
+    })
+
+    // Splene ulohy
+    if(this.todos[index].completed){
+      this.taskDone = this.taskDone + 1
+      this.progress = ((this.taskDone / this.todos.length) * 100)
+    } 
+
+    if(this.todos[index].completed === false){
+      this.taskDone = this.taskDone - 1
+      this.progress = ((this.taskDone / this.todos.length) * 100)
+    } 
+
+    // Zmena farieb
+    if(this.progress < 35){
+      return this.color = 'warn'
+    } else if(this.progress < 70){
+      return this.color = 'accent'
+    } else {
+      return this.color = 'primary'
+    }
+  }
+
+  deleteAll(){
+    this.todos = []
+    this.taskDone = 0
+    this.progress = 0
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
